9405bb0d1b2eee2451c2fdca4fe2b0476ba0bb16
Souzaa <129308622+Hominho45545@users.noreply.github.com>
2024-10-10 08:38:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const botões = document. querySelectorAll(".botao");
 const textos = document. querySelectorAll(".aba-conteudo");
 
@@ -26,4 +25,33 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function calcularTempo(tempoObjetivo){
     let tempoAtual = new Date ();
     let tempoFinal = tempoObjetvio - tempoAtual;
+    let segundos = math.floor(tempoFinal / 1000);
+    let minutos = math.floor(segundos / 60 );
+    let horas = math.floor(minutos / 60 );
+    let dias = math.floor(horas / 24 );
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+if (tempoFinal > 0);{
+        return [dias,horas,minuos,segundos];
+    }else {
+        return [0,0,0,0];
+    }
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
+
+    function comecaCronometro() {
+        atualizaCronometro();
+        setInterval(atualizaCronometro, 1000);
+    }
+comecaCronometro();
